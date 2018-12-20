465dcbfb6fad7989db611e6be3cadf610b3313d0
BenjaminAce <pawlo97.pb@gmail.com>
2018-12-20 17:19:19 +0100

    Contructors in inherited classes

---
+++ HeadFirst/MyBaseClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeadFirst
+{
+    public class MyBaseClass
+    {
+        public MyBaseClass(string baseClassNeedsThis)
+        {
+            MessageBox.Show("to jest klasa bazowa: " + baseClassNeedsThis);
+        }
+    }
+}
---
+++ HeadFirst/MySubclass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirst
+{
+    public class MySubclass : MyBaseClass
+    {
+        public MySubclass(string baseClassNeedsThis, int anotherValue) : base(baseClassNeedsThis)
+        {
+
+        }
+        
+    }
+}
